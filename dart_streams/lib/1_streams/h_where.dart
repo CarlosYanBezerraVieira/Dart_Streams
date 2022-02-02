270eebcb84a0378006eb13ed5dc8c75b42710361
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2022-02-02 14:29:15 -0300

    Where in stream

--- dart_streams/lib/1_streams/h_where.dart
+++ dart_streams/lib/1_streams/h_where.dart
@@ -2,8 +2,7 @@ Future<void> main(List<String> args) async {
   print("inicio");
   final interval = Duration(seconds: 1);
   var stream = Stream<int>.periodic(interval, callback);
-  //filtra os valores que atendem a condição
-  stream = stream.where((valor) => valor % 6 == 0).take(3);
+  stream = stream.take(10);
 //realiza o evento do seu escopo toda vez que a variavel é modificada
   stream.listen((numero) {
     print("Listen value: $numero");
@@ -14,5 +13,5 @@ Future<void> main(List<String> args) async {
 
 int callback(int valor) {
   print("valor callback : $valor");
-  return (valor + 1) * 2;
+  return valor * 2;
 }
